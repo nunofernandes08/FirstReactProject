063551e244dcc29c902d0b9cbf5f61fc3fc115fb
Nuno Fernandes <ebm4562@live.com>
2021-05-06 16:36:01 +0100

    textfileds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
 import './App.css';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Navbar from './components/navbar/navbar';
 import Table from './components/table/table';
 import Cards from './components/cards/cards';
-
 import Buttons from './components/buttons/buttons';
+import Textfields from './components/textfields/textfields';
 
 
 function App() {
@@ -27,6 +30,36 @@ function App() {
       </div>
       <h1 class="h1" >MY BUTTONS</h1>
       <Buttons />
+      <h1 class="h1" >MY TEXTFIELDS</h1>
+      <Textfields />
+      <h1 class="h1" >MY ROUTING</h1>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <Table />
+          </Route>
+          <Route path="/users">
+            <Cards />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
     </div>
   );
 }
